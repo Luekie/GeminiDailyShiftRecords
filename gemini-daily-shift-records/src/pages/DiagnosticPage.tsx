9ed6ed25723ee49397ae4949eed0ae2be7ccb4d0
Luekie <bsc-com-03-22@unima.ac.mw>
2026-01-24 20:21:35 +0200

    production2

--- gemini-daily-shift-records/src/pages/DiagnosticPage.tsx
+++ gemini-daily-shift-records/src/pages/DiagnosticPage.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Card, CardContent } from '@/components/ui/card';
 import { useTheme } from '../contexts/ThemeContext';
 import { GlobalBackground } from '../components/GlobalBackground';
